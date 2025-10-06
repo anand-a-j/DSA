7647bf94f2d851b5502a6bcfceb9d0e192a90699
Anand A J <132826472+anand-a-j@users.noreply.github.com>
2025-10-06 07:58:56 +0530

    Time: 0 ms (100.00%) | Memory: 152.6 MB (8.74%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.dart
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.dart
@@ -1,16 +1,29 @@
- // 06/10/2025
- class Solution {
-   int removeDuplicates(List<int> nums) {
-    List<int> k = [];
-    for(int i = 1; i<nums.length;i++) {
-       if(nums[i] == nums[i-1]){
-         nums.removeAt(i);
-         i=0;
-       }
+class Solution {    
+  int removeDuplicates(List<int> nums) {
+   int nextWriteIndex = 1;
+   for(int i = 1;i<nums.length;i++){
+    if(nums[i] != nums[i-1]){
+        nums[nextWriteIndex] = nums[i];
+        nextWriteIndex++;
     }
-    return nums.length;
    }
+   return nextWriteIndex;
+ }
 }
+
+//  // 06/10/2025
+//  class Solution {
+//    int removeDuplicates(List<int> nums) {
+
+//     for(int i = 1; i<nums.length;i++) {
+//        if(nums[i] == nums[i-1]){
+//          nums.removeAt(i);
+//          i=0;
+//        }
+//     }
+//     return nums.length;
+//    }
+// }
 
 
 
