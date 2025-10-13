63afb8f835fdc6154822cbefde6055d80216c594
Anand A J <132826472+anand-a-j@users.noreply.github.com>
2025-10-13 07:44:46 +0530

    Time: 1 ms (76.19%) | Memory: 153.7 MB (7.14%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.dart
+++ 2058-concatenation-of-array/concatenation-of-array.dart
@@ -1,8 +1,33 @@
+// trying with loop - 13/10/2025
 class Solution {
   List<int> getConcatenation(List<int> nums) {
     List<int> ans = [];
-    ans.addAll(nums);
-    ans.addAll(nums);
+    int j = 0;
+
+    for(int i=0;i<nums.length*2;i++){
+        ans.add(nums[j]);
+
+        if(j==nums.length-1){
+            j=0;
+        }else{
+            j++;
+        }
+    }
     return ans;
   }
 }
+
+
+
+
+
+
+
+// class Solution {
+//   List<int> getConcatenation(List<int> nums) {
+//     List<int> ans = [];
+//     ans.addAll(nums);
+//     ans.addAll(nums);
+//     return ans;
+//   }
+// }
