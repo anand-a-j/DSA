9d03a685c0574b6ff0bc15887d86f5000cd703ec
Anand A J <132826472+anand-a-j@users.noreply.github.com>
2025-10-15 09:22:16 +0530

    Time: 0 ms (100.00%) | Memory: 150.4 MB (35.00%) - LeetSync

---
+++ 27-remove-element/remove-element.dart
@@ -0,0 +1,16 @@
+
+
+
+class Solution {
+  int removeElement(List<int> nums, int val) {
+   int index = 0;
+
+   for(int i=0;i<nums.length;i++){
+    if(nums[i] != val){
+        nums[index] = nums[i];
+        index ++;
+    }
+   }
+   return index;
+  }
+}
