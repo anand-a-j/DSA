69c8952b798d8ce42a2a7cf289176a428a2a2336
Anand A J <132826472+anand-a-j@users.noreply.github.com>
2025-11-08 08:10:11 +0530

    Time: 1 ms (99.28%) | Memory: 171 MB (72.66%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.dart
+++ 11-container-with-most-water/container-with-most-water.dart
@@ -1,23 +1,69 @@
 class Solution {
-  int maxArea(List<int> height) {
-    int l = 0;
-    int r = height.length - 1;
+ int maxArea(List<int> height){   
+    int left = 0;
+    int right = height.length-1;
     int area = 0;
 
-    while(r>l){
-     int cArea = (r-l) * min(height[l],height[r]);
+    while(left<right) {
+        int cArea = (right-left) * min(height[left],height[right]);
 
-     if(cArea>area){
-        area = cArea;
-     }
-      
-     if(height[r] > height[l]){
-        l++;
-     }else{
-        r--;
-     }
-        
-    }
-    return area;
+        if(cArea>area){
+            area = cArea;
+        }
+
+        if(height[left]>height[right]){
+            right--;
+        }else{
+         left++;
+        }
+    }  
+
+    return area;                                                                                                                                                                                                                            
   }
-}
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//   int maxArea(List<int> height) {
+//     int l = 0;
+//     int r = height.length - 1;
+//     int area = 0;
+
+//     while(r>l){
+//      int cArea = (r-l) * min(height[l],height[r]);
+
+//      if(cArea>area){
+//         area = cArea;
+//      }
+      
+//      if(height[r] > height[l]){
+//         l++;
+//      }else{
+//         r--;
+//      }
+        
+//     }
+//     return area;
+//   }
+// }
