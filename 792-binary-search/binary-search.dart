df44b86bc28693052ebd2320a1772a86632b3fc5
Anand A J <132826472+anand-a-j@users.noreply.github.com>
2025-11-13 07:59:43 +0530

    Time: 0 ms (100.00%) | Memory: 158.2 MB (9.52%) - LeetSync

---
+++ 792-binary-search/binary-search.dart
@@ -0,0 +1,62 @@
+class Solution {
+  int search(List<int> nums, int target) {
+   int left = 0;
+   int right = nums.length-1;
+
+   while(left<=right){
+    int mid = left + (right-left) ~/ 2;
+
+    if(nums[mid]==target){
+        return mid;
+    }else if(target> nums[mid]){
+        left = mid + 1;
+    }else{
+        right = mid - 1;
+    }
+   }
+
+   return -1;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//   int search(List<int> nums, int target) {
+//     int start = 0;
+//     int end = nums.length - 1;
+
+//     while(start <= end) {
+//         int mid = start + (end - start) ~/ 2;
+
+//         if(target > nums[mid]) {
+//             start = mid + 1;
+//         }else if(target < nums[mid]) {
+//             end = mid - 1;
+//         }else if(target == nums[mid]) {
+//             return mid;
+//         }
+//     }
+
+//     return -1;
+//   }
+// }
